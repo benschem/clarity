fbe3d903c35239d2dbd48062d81426d5c52cd1fc
Ben Schembri <benschembri@gmail.com>
2025-06-19 16:47:53 +1000

    move sort metod elsewhere and change puts to warn

--- lib/repositories/project_repository.rb
+++ lib/repositories/project_repository.rb
@@ -23,23 +23,6 @@ module Clarity
       @projects.find { |project| project.id == id }
     end
 
-    def sort_projects(projects, sort_param)
-      projects.sort_by! do |project|
-        case sort_param
-        when 'name'       then project.name.downcase
-        when 'status'     then Clarity::Project::STATUS_ORDER[project.status.to_sym] || 999
-        when 'urgency'    then Clarity::Project::URGENCY_ORDER[project.urgency.to_sym] || 999
-        when 'type'       then Clarity::Project::TYPE_ORDER[project.type.to_sym] || 999
-        when 'motivation' then Clarity::Project::MOTIVATION_ORDER[project.motivation.to_sym] || 999
-        when 'created_at' then time(project.created_at)
-        when 'pushed_at'  then time(project.pushed_at)
-        else
-          Clarity::CoreView.usage_help
-          nil # don't sort by anything
-        end
-      end
-    end
-
     def save(project)
       metadata = {
         status: project.status,
@@ -52,13 +35,13 @@ module Clarity
         FileUtils.mkdir_p(File.dirname(metadata_path))
         File.write(metadata_path, JSON.pretty_generate(metadata))
       rescue SystemCallError, IOError => e
-        puts "Error: Failed to write metadata: #{e.message}"
+        warn "Error: Failed to write metadata: #{e.message}"
       end
     end
 
     # def new(name)
     #   if load(name)
-    #     puts "Project '#{name}' already exists."
+    #     warn "Project '#{name}' already exists."
     #     exit 1
     #   end
 
@@ -84,12 +67,12 @@ module Clarity
     # def finish(name)
     #   project = load(name)
     #   unless project
-    #     puts "Project '#{name}' not found."
+    #     warn "Project '#{name}' not found."
     #     exit 1
     #   end
 
     #   if project['status'] == 'finished'
-    #     puts "Project '#{name}' is already finished."
+    #     warn "Project '#{name}' is already finished."
     #     exit 1
     #   end
 
@@ -106,13 +89,6 @@ module Clarity
     # end
     #
     private
-
-    def time(timestamp)
-      Time.parse(timestamp.to_s)
-    rescue ArgumentError, TypeError
-      puts "Error: Failed to parse Timestamp: #{e.class} - #{e.message} "
-      nil
-    end
 
     def load_all_projects
       Dir.glob("#{PROJECTS_DIR}/*.json").map do |file|
@@ -156,7 +132,7 @@ module Clarity
         FileUtils.mkdir_p(File.dirname(metadata_path))
         File.write(metadata_path, JSON.pretty_generate(metadata))
       rescue SystemCallError, IOError => e
-        puts "Error: Failed to write metadata: #{e.message}"
+        warn "Error: Failed to write metadata: #{e.message}"
       end
     end
   end
