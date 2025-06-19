f5cad295cc7a697ba703107b5743e6c27a2b08d0
Ben Schembri <benschembri@gmail.com>
2025-06-19 16:44:54 +1000

    refactor to an instance based class and break out methods in display_list

--- lib/views/projects.rb
+++ lib/views/projects.rb
@@ -6,24 +6,13 @@ module Clarity
   class ProjectsView
     MAX_DESCRIPTION_LENGTH = 60
 
-    def self.list(projects)
+    def display_list(projects)
       if projects.empty?
         puts 'No projects found.'
         return
       end
 
-      rows = projects.map do |project|
-        id = project.id
-        name = project.name
-        description = project.description
-        description = "#{description[0..60]}..." if description&.length&.> MAX_DESCRIPTION_LENGTH
-        status = project.status ? Clarity::Project::STATUSES[project.status.to_sym] : ''
-        urgency = project.urgency ? Clarity::Project::URGENCIES[project.urgency.to_sym] : ''
-        type = project.type ? Clarity::Project::TYPES[project.type.to_sym] : ''
-        motivation = project.motivation ? Clarity::Project::MOTIVATIONS[project.motivation.to_sym] : ''
-
-        [id, status, urgency, name, type, description, motivation]
-      end
+      rows = create_rows(projects)
 
       table = Terminal::Table.new do |t|
         t.headings = %w[id stts urgc name type description mtvn]
@@ -36,6 +25,42 @@ module Clarity
       end
 
       puts table
+    end
+
+    private
+
+    def create_rows(projects)
+      projects.map do |project|
+        id = project.id
+        name = project.name
+        description = create_description(project)
+        status = create_status(project)
+        urgency = create_urgency(project)
+        type = create_type(project)
+        motivation = create_motivation(project)
+
+        [id, status, urgency, name, type, description, motivation]
+      end
+    end
+
+    def create_description(project)
+      "#{project.description[0..60]}..." if project.description&.length&.> MAX_DESCRIPTION_LENGTH
+    end
+
+    def create_status(project)
+      project.status ? Clarity::Project::STATUSES[project.status.to_sym] : ''
+    end
+
+    def create_urgency(project)
+      project.urgency ? Clarity::Project::URGENCIES[project.urgency.to_sym] : ''
+    end
+
+    def create_type(project)
+      project.type ? Clarity::Project::TYPES[project.type.to_sym] : ''
+    end
+
+    def create_motivation(project)
+      project.motivation ? Clarity::Project::MOTIVATIONS[project.motivation.to_sym] : ''
     end
   end
 end
